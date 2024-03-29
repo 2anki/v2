f4942513998d9f035728ad727a7ee3a165ef3c7e
Alexander Alemayhu <alexander@alemayhu.com>
2024-03-29 19:44:07 +0100

    add ios app

---
+++ mobile/2anki_ios/2anki_ios/_anki_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  _anki_iosApp.swift
+//  2anki_ios
+//
+//  Created by Alexander Alemayhu on 29/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct _anki_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
